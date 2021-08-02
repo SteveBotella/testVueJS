c0542b5d14cf08f62931296517f36afac8544efd
Steve <stevebotella@hotmail.fr>
2021-08-02 16:20:03 +0200

    feat: class for color

--- ressources/js/script.js
+++ ressources/js/script.js
@@ -1,24 +1,16 @@
 const UpdateH1 = {
     data() {
         return {
-            updateH1: 'Truc bidule',
-            updateH2: 'Truc machin',
-            textH2: 'text h2',
-            textColor: 'black',
-            checked: false,
-            checkedImg: false,
+            h1: 'Truc bidule',
+            h2: 'text h2',
+            h2Temp: '',
+            isImageVisible: false,
+            isClassColorActive: false,
         }
     },
     methods: {
-        clickH2: function () {
-            this.textH2 = this.updateH2
-        },
-        changeTextColor: function () {
-          if (this.checked) {
-              this.textColor = 'black'
-          } else {
-              this.textColor = 'red'
-          }
+        replaceH2Text: function () {
+            this.h2 = this.h2Temp
         }
     }
 }
