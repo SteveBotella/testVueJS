81742428aafefe1e4595828ce12f17c7cc17dada
Steve <stevebotella@hotmail.fr>
2021-08-02 11:27:05 +0200

    feat: new vue + el + data + v-if
    Show list & messages with condition

---
+++ ressources/js/script.js
@@ -0,0 +1,9 @@
+new Vue({
+    el: '#app',
+    data: {
+        message: 'Coucou',
+        link: 'https://www.youtube.com/watch?v=XkgiXngcpWk',
+        success: true,
+        persons: ['LeJoe', 'LeDavid', 'Lehoffman', 'LaCalotteDeSesMobs']
+    }
+})
