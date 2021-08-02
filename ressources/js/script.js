efb0ffbe0f1df040a1c9c414d2966a687a5b5180
Steve <stevebotella@hotmail.fr>
2021-08-02 15:46:49 +0200

    feat: end

--- ressources/js/script.js
+++ ressources/js/script.js
@@ -1,9 +1,26 @@
-const Message = {
+const UpdateH1 = {
     data() {
         return {
-            message: "Truc bidule"
+            updateH1: 'Truc bidule',
+            updateH2: 'Truc machin',
+            textH2: 'text h2',
+            textColor: 'black',
+            checked: false,
+            checkedImg: false,
+        }
+    },
+    methods: {
+        clickH2: function () {
+            this.textH2 = this.updateH2
+        },
+        changeTextColor: function () {
+          if (this.checked) {
+              this.textColor = 'black'
+          } else {
+              this.textColor = 'red'
+          }
         }
     }
 }
 
-Vue.createApp(Message).mount('#message')
+Vue.createApp(UpdateH1).mount('#update')
